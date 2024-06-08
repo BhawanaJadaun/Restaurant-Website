5b08be607752e002eb15f5d75c85666daa339b73
BhawanaJadaun <154340487+BhawanaJadaun@users.noreply.github.com>
2024-06-08 18:23:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-  build: {chunkSizeWarningLimit:1600,}
-})
+  build: {
+    rollupOptions: {
+      external: ['bootstrap']
+    }
+  }
+});
